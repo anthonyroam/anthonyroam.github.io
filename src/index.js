f5ec808b4f4f2358e80557b45193e6968d32fdc9
anthonyroam <anthonyroam2@gmail.com>
2022-06-17 02:44:16 -0400

    first version

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const project = document.querySelectorAll(".project__container");
 const projectArray = [...project];
 const projectPusher = document.querySelectorAll(".pusher");
 const projectPusherArray = [...projectPusher]
+const themeButton = document.querySelectorAll(".themeButton");
+const themeButtonArray = [...themeButton];
+const skillset = document.getElementById("skillset");
 
 menuIcon.addEventListener( "click", (e) => {
     menu.classList.toggle("show")
@@ -16,3 +19,29 @@ projectArray.map((project, index) => {
 projectArray.map((project, index) => {
     project.addEventListener("mouseleave", () => projectPusherArray[index].classList.remove("push"));
 })
+
+themeButtonArray.map(themeButton => {
+    themeButton.addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+        
+        themeButton.innerText === "Light mode"
+        ? themeButton.innerText = "Dark mode"
+        : themeButton.innerText = "Light mode"
+    })
+})
+
+const observer = new IntersectionObserver(handlerIntersection, {
+    threshold: 0.01,
+});
+
+observer.observe(skillset);
+
+function handlerIntersection(entries) {
+    const entry = entries[0];
+
+    const isVisible = entry.intersectionRatio >= 0.01;
+
+    if (isVisible) {
+        skillset.classList.add("animation");
+    }
+};
